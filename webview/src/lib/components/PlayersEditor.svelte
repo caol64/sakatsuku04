e14d151391770498655ee594c7295d5b0bf8b8c6
Lei <caol64@gmail.com>
2025-08-05 13:35:07 +0800

    add

--- webview/src/lib/components/PlayersEditor.svelte
+++ webview/src/lib/components/PlayersEditor.svelte
@@ -315,7 +315,7 @@
         @apply w-30 bg-transparent placeholder:text-slate-400 text-slate-700 dark:text-gray-300 text-sm border border-slate-200 rounded pl-3 pr-8 py-2 transition duration-300 focus:outline-none focus:border-slate-400 hover:border-slate-400 shadow-sm focus:shadow-md;
     }
     .thin-select {
-        @apply w-20 bg-transparent placeholder:text-slate-400 text-slate-700 dark:text-gray-300 text-sm border border-slate-200 rounded pl-3 pr-8 py-2 transition duration-300 focus:outline-none focus:border-slate-400 hover:border-slate-400 shadow-sm focus:shadow-md appearance-none cursor-pointer;
+        @apply w-22 bg-transparent placeholder:text-slate-400 text-slate-700 dark:text-gray-300 text-sm border border-slate-200 rounded pl-3 pr-8 py-2 transition duration-300 focus:outline-none focus:border-slate-400 hover:border-slate-400 shadow-sm focus:shadow-md appearance-none cursor-pointer;
     }
     .form {
         @apply grid grid-cols-[1fr_2fr] my-2 items-center;
